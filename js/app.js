cb97b4dd30c9673fa92cad44dcffb6e81c19fbcc
Ayush Sharma <ayush.aceit@gmail.com>
2015-04-06 05:15:49 +0530

    card prop updated

--- js/app.js
+++ js/app.js
@@ -17,27 +17,43 @@ angular.module('ayush',['ngRoute'])
 		//controller:'HomeCtrl',
 		templateUrl:'templates/projects.html'
 	})
+	.when('/resume',{
+		//controller:'HomeCtrl',
+		templateUrl:'templates/resume.html'
+	})
+	.when('/milestone',{
+		// controller:'HomeCtrl',
+		templateUrl:'templates/milestones.html'
+	})
+	.when('/contact',{
+		// controller:'HomeCtrl',
+		templateUrl:'templates/contact.html'
+	})
 })
 
 //controllers
 
-// .controller('HomeCtrl',function($scope){
-
-// })
+.controller('CollapsibleCtrl',function($scope){
+	$(document).ready(function(){
+		$('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+	});
+})
 
 //directives
 .directive('header', [function(){
-    var directive = {
-        restrict: 'EA',
-        templateUrl: 'templates/directive/header.html'
-    }
-  return directive;
+	var directive = {
+		restrict: 'EA',
+		templateUrl: 'templates/directive/header.html'
+	}
+	return directive;
 }])
 
 .directive('footer', [function(){
-    var directive = {
-        restrict: 'EA',
-        templateUrl: 'templates/directive/footer.html'
-    }
-  return directive;
+	var directive = {
+		restrict: 'EA',
+		templateUrl: 'templates/directive/footer.html'
+	}
+	return directive;
 }]);
