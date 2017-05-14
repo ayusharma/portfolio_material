a93816c0afd02696cfc61c59810680285decfc98
Ayush Sharma <ayush.sharma@innovaccer.com>
2017-05-14 19:35:19 +0530

    loader fixes

--- js/app.js
+++ js/app.js
@@ -27,11 +27,13 @@ angular.module('ayush',['ngRoute','ngSanitize'])
 .controller('WritingController',function($scope, $http){
 	var url = 'http://morning-earth-19323.herokuapp.com/?feedURL=https://medium.com/feed/@ayusharma.in';
 	$scope.writing = {};
+	$scope.loading = true;
 	$http({
 	  method: 'GET',
 	  url: url
 	}).then(function successCallback(response) {
 	     $scope.writing.data = response.data;
+			 $scope.loading = false;
 	  }, function errorCallback(error) {
 	    console.log(error)
 	});
@@ -40,22 +42,23 @@ angular.module('ayush',['ngRoute','ngSanitize'])
 .controller('ProjectsController', function ($scope, $http) {
 	var url = 'https://spreadsheets.google.com/feeds/list/1jYueR8BrT3qvvYCh7_tbH9wBXr-CbNzXiV_dZSAXpGQ/od6/public/values?alt=json';
 	$scope.projects = {};
+	$scope.loading = true;
 	$http({
 	  method: 'GET',
 	  url: url
 	}).then(function successCallback(response) {
 	     $scope.projects.data = response.data;
+			 $scope.loading = false;
 	  }, function errorCallback(error) {
 	    console.log(error)
 	});
 })
 
-.controller('ResumeController', function ($scope, loader) {
+.controller('ResumeController', function ($scope) {
 	// If absolute URL from the remote server is provided, configure the CORS
 	// header on that server.
-	loader.turnOn();
+	$scope.loading = true;
 	var url = 'https://docs.google.com/document/d/1S8OKfwODNMx--Rj2yevQ1hXnON4hc2qTCdw4JNZPfh4/export?format=pdf';
-
 	// Disable workers to avoid yet another cross-origin issue (workers need
 	// the URL of the script to be loaded, and dynamically loading a cross-origin
 	// script does not work).
@@ -70,7 +73,7 @@ angular.module('ayush',['ngRoute','ngSanitize'])
 	  // Fetch the first page
 	  var pageNumber = 1;
 	  pdf.getPage(pageNumber).then(function(page) {
-
+			// loader.turnOff();
 	    var scale = 1.5;
 	    var viewport = page.getViewport(scale);
 
@@ -87,8 +90,9 @@ angular.module('ayush',['ngRoute','ngSanitize'])
 	    };
 	    var renderTask = page.render(renderContext);
 	    renderTask.then(function () {
+				$scope.loading = false;
+				$scope.$apply();
 	      console.log('Page rendered');
-				loader.turnOff();
 	    });
 	  });
 	}, function (reason) {
@@ -119,49 +123,14 @@ angular.module('ayush',['ngRoute','ngSanitize'])
 	return directive;
 }])
 
-.directive('loading', ['$http', 'loader', function($http, loader){
+.directive('loading', [function(){
 	var directive = {
 		restrict: 'EA',
-		templateUrl: 'templates/directive/loading.html',
-		link: function (scope, elm, attrs) {
-
-			loader.setLoader(elm);
-
-      scope.isLoading = function () {
-          return $http.pendingRequests.length;
-      };
-      scope.$watch(scope.isLoading, function (v) {
-        if(v){
-            elm.show();
-        }else{
-            elm.hide();
-        }
-    	});
-    }
+		templateUrl: 'templates/directive/loading.html'
 	}
 	return directive;
 }])
 
-.factory('loader', function () {
-	var elmement;
-
-	var setLoader = function (elm) {
-		elmement = elm;
-	}
-	var turnOn = function () {
-		elmement.show();
-	};
-
-	var turnOff = function () {
-		elmement.hide();
-	};
-
-	return {
-		setLoader: setLoader,
-		turnOn: turnOn,
-		turnOff: turnOff
-	};
-})
 .run(function ($rootScope, $location) {
 	$rootScope.$on('$locationChangeSuccess', function () {
 		$rootScope.path = $location.path();
